1c95c15fe2aacbe425025cfe5aa3285f1b7c39e0
laxmi-duggineni <70114536+laxmi-duggineni@users.noreply.github.com>
2021-08-31 05:57:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC-jhrBRJrj1ZQ_uinIADreDs1TFB63dMs",
+    authDomain: "c-85--auth.firebaseapp.com",
+    projectId: "c-85--auth",
+    storageBucket: "c-85--auth.appspot.com",
+    messagingSenderId: "753405876929",
+    appId: "1:753405876929:web:01f6b66ac57a70db6bd240"
+  };
+  
